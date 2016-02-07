abf762d71269f838e13cb9d01b05a465aab9d7d7
Kurt Neufeld <kneufeld@burgundywall.com>
2016-02-06 22:34:06 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+name = 'django-dynamicstatics'
+package = 'dynamicstatics'
+description = "A Django utility application that dynamically changes MEDIA_URL and STATIC_URL"
+url = 'https://github.com/kneufeld/django-dynamicstatics'
+author = 'Kurt Neufeld'
+author_email = 'kneufeld@burgundywall.com'
+license = 'MIT'
+keywords = ['django','media_url','static_url']
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Utilities'
+]
+install_requires=[
+          'iptools>=0.4.0',
+          'django-ipware>=1.1.3',
+      ],
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    args = {'version': get_version(package)}
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %(version)s -m 'version %(version)s' && git push --tags" % args)
+    sys.exit()
+
+
+setup(
+    name=name,
+    version=get_version(package),
+    url=url,
+    license=license,
+    description=description,
+    author=author,
+    author_email=author_email,
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=install_requires,
+    keywords=keywords,
+    classifiers=classifiers
+)
+
